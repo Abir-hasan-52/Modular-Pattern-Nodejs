e9f022c3aa6a2dd48592102e325040abd37464fd
Abir Hasan <abirhasan5208@gmail.com>
2026-05-22 10:12:16 +0600

    create user profile with module pattern

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import express, {
 
 import { pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
+import { profileRoute } from "./modules/profile/profile.route";
 
 const app: Application = express();
 
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true })); // for nested data to show we use "extended = true" . if it false then it can't to show nested data
 
-app.use("/api/users", userRoute)
+
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
@@ -22,6 +23,11 @@ app.get("/", (req: Request, res: Response) => {
     author: "Abir",
   });
 });
+
+app.use("/api/users", userRoute);
+app.use("/api/profile", profileRoute);
+
+ 
 
  
  
