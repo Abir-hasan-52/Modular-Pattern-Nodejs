8a6b7008eeb72236af73f84520e96f23ee1ad04e
Abir Hasan <abirhasan5208@gmail.com>
2026-05-22 15:00:23 +0600

    make a custiom middleware

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import express, {
   type Response,
 } from "express";
 
-import { pool } from "./db";
+import fs from "fs";
 import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRouter } from "./modules/auth/auth.route";
@@ -15,6 +15,25 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true })); // for nested data to show we use "extended = true" . if it false then it can't to show nested data
+
+app.use((req, res, next) => {
+  const humanReadableTime = new Date().toISOString();
+
+  // 1. Added a newline (\n) at the end so every log gets its own row
+  const log = `Method: ${req.method} | URL: ${req.url} | Time: ${humanReadableTime}\n`;
+
+  // Clearer console log for debugging
+  console.log(`[LOG] ${req.method} -> ${req.url}`);
+
+  // 2. Wrap error logging in an 'if' statement to avoid printing 'null'
+  fs.appendFile("logger.txt", log, (err) => {
+    if (err) {
+      console.error("Error writing to log file:", err);
+    }
+  });
+
+  next();
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
